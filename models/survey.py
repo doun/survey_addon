3db8e0026d3e3b401d948542c266ff4131640737
doun <doun@163.com>
2018-09-24 22:55:29 +0800

    [WIP] pack answer line to csv

--- models/survey.py
+++ models/survey.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+import csv
 import datetime
 import logging
 import re
@@ -84,7 +85,7 @@ class SurveyInputLine(models.Model):
                             'datas_fname': question.file_name,
                             'res_model': 'survey.user_input',
                             'datas': base64.b64encode(content),
-                            #'res_field': question.id,
+                            # 'res_field': question.id,
                             'res_id': user_input_id
                         }
                         att = Attach.create(att)
@@ -102,9 +103,52 @@ class SurveyUserInput(models.Model):
     _inherit = 'survey.user_input'
     survey_id = fields.Many2one('survey.survey', readonly=False)
     partner_id = fields.Many2one('res.partner', readonly=False)
+    task_id = fields.Many2one('project.task', string='附加到任务')
     state = fields.Selection(readonly=False)
     type = fields.Selection(readonly=False)
     email = fields.Char(readonly=False)
+
+    def to_csv_line(self, line):
+        title = line.question_id.question
+        val = ""
+        if 'text' in str(line.answer_type):
+            val = line.value_text
+        if line.answer_type == 'date':
+            val = str(line.value_date)
+        if line.answer_type == 'suggestion':
+            val = line.question_id.label_ids[line.value_suggested_row].value
+        if line.answer_type == 'number':
+            val = string(line.value_number)
+        return (title, val)
+
+
+    @api.multi
+    def pack_as_attach(self):
+        self.ensure_one()
+        Attach = self.env['ir.attachment']
+        # TODO: Step1. Gen csv from inputs
+        csv_f = csv.StringIO()
+        csv_w = csv.DictWriter(csv_f, ['title', 'value'])
+        lines = [self.to_csv_line(l) for l in self.user_input_line_ids if l.question_id.type != 'attach' and l.skipped = False]
+        for t, v in lines:
+            csv_w.writerow({'title': t, 'value': v})
+
+        att = {
+            'name': self.question_id.file_name,
+            'datas_fname': self.question_id.file_name,
+            'res_model': 'survey.user_input',
+            'datas': base64.b64encode(content),
+            # 'res_field': question.id,
+            'res_id': user_input_id
+        }
+        csv_att = Attach.create(att)
+
+        # Step2. Gen zip
+        return
+
+    @api.multi
+    def action_sendto_provider(self):
+        return
 
     @api.multi
     def action_open_url(self):
